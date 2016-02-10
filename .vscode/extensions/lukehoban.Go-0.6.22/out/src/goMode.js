b017b4d0ebe5da71adb8b58379e33a6f42e37d4e
gngeorgiev <gngeorgiev.it@gmail.com>
2016-02-10 19:22:03 +0200

    removed vscode

--- .vscode/extensions/lukehoban.Go-0.6.22/out/src/goMode.js
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-'use strict';
-exports.GO_MODE = { language: 'go', scheme: 'file' };
-//# sourceMappingURL=goMode.js.map
